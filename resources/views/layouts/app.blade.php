0e601537e17a2a2f6ff2f93b6988228585890d3a
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-24 17:00:42 +0200

    fixed app.blade.php in layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{ $title }}</title>
+        @yield("additional-meta")
+        <title>@yield("page-title", "laravel-comics")</title>
+
+        @yield("additional-cdn")
     </head>
+
     <body>
         {{-- header --}}
         @include('partials.header')
@@ -17,8 +21,7 @@
 
         {{-- footer --}}
         @include('partials.footer')
-
         @vite('resources/js/app.js')
-
     </body>
+
 </html>
